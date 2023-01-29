7f2b93b7231fa02a41ef58d30ccbd425f1867e8a
LAPTOP-TBQR1IVE\KD <pucabuca23@gmail.com>
2023-01-29 21:55:08 +0100

    no message

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ var table = document.querySelector("#tabela");
 
 addbutton.addEventListener('click',addrow);
 
+var tablica = [{name: "Dzień", date: moment().format('dddd Do')}, {name: "Miesiąc", date: moment().format('MMMM')},{name: "Rok", date: moment().format('YYYY') }];
+
 function addrow() {
    var tablelength = table.rows.length;
 
+   for (var i=0; i<tablica.length; i++){
    var row = table.insertRow(tablelength);
+
    var cel1 = row.insertCell(0);
-   cel1.innerHTML = "komorka1";
+   cel1.innerHTML = tablica[i].name;
 
    var cel2 = row.insertCell(1);
-   cel2.innerHTML = "komorka2";
+   cel2.innerHTML = tablica[i].date;
+   }
 }
